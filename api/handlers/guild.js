0db6b4c52a5c5b51349f1e3a5c5b3bcc6eca691d
Pieface <aabhinav682@gmail.com>
2021-05-11 14:31:33 +0530

    add 404 response to get guild

--- api/handlers/guild.js
+++ api/handlers/guild.js
@@ -1,6 +1,5 @@
 const db = require("../../models");
 const { withTransaction, asyncMiddleware } = require("../../middlewares/utils");
-const mongoose = require("mongoose");
 
 const createGuild = withTransaction(async (session, req, res) => {
   const { name, createdBy, categories } = req.body;
@@ -22,9 +21,10 @@ const createGuild = withTransaction(async (session, req, res) => {
 });
 
 const getGuild = asyncMiddleware(async (req, res) => {
-  console.log(req.params.guildId);
   const guild = await db.Guild.findById(req.params.guildId).lean().exec();
-  console.log(guild instanceof mongoose.Document);
+  if (!guild) {
+    res.status(404).send("Guild does not exist.");
+  }
 
   res.json(guild);
 });
